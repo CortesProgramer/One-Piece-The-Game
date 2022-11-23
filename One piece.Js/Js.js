0d6bd21b2b68071eb09da0cca47e1393adbb156d
Simón Cortés <118238331+CortesProgramer@users.noreply.github.com>
2022-11-23 12:49:46 -0500

    span js

--- One piece.Js/Js.js
+++ One piece.Js/Js.js
@@ -6,13 +6,17 @@ function iniciarJuego() {
     botonPirata.addEventListener("click", seleccionarPirataJugador)
 }
 
-function seleccionarPirataJugador() {   
-
+function seleccionarPirataJugador() { 
+    let spanPirataJugador = document.getElementById("pirataJugador")
+     
     if (document.getElementById("Monkey D. Luffy").checked) {
+        spanPirataJugador.innerHTML = "Luffy" 
         alert("Seleccionaste a Mugiwara No Luffy")
     } else if (document.getElementById("Akagami No Shanks").checked) {
+        spanPirataJugador.innerHTML = "Shanks" 
         alert("Seleccionaste a Akagami No Shanks")
     } else if (document.getElementById("Portgas D. Ace").checked) {
+        spanPirataJugador.innerHTML = "Ace" 
         alert("Seleccionaste a Hiken No Ace")
     } else{ 
         alert("Selecciona un personaje")
