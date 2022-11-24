07c0778cf19a4fdff0e8a66872f606ff5f9cdc0d
Simón Cortés <118238331+CortesProgramer@users.noreply.github.com>
2022-11-24 17:47:51 -0500

    a

--- One piece.Js/Js.js
+++ One piece.Js/Js.js
@@ -1,3 +1,9 @@
+let ataqueJugador 
+
+
+
+
+
 function aleatorio(min, max) {
     return Math.floor ( Math.random() * (max - min + 1) + min)
 }
@@ -6,7 +12,14 @@ function aleatorio(min, max) {
 function iniciarJuego() {
     let botonPirata = document.getElementById("boton-pirata")
     botonPirata.addEventListener("click", seleccionarPirataJugador)
-}
+
+        let botonGolpe = document.getElementById("botonGolpe")
+                botonGolpe.addEventListener("click", ataqueGolpe)
+        let botonPatada = document.getElementById("botonPatada")
+                botonPatada.addEventListener("click", ataquePatada)
+        let botonCabezazo = document.getElementById("botonCabezazo")
+                botonCabezazo.addEventListener("click", ataqueCabezazo)    
+}   
 
 function seleccionarPirataJugador() { 
     let spanPirataJugador = document.getElementById("pirataJugador")
@@ -40,5 +53,28 @@ function seleccionarPirataPc() {
         spanPirataPc.innerHTML = "Ace"
     }
 }
+
+function ataqueGolpe() {
+    ataqueJugador = "Golpe"
+}   alert(ataqueJugador)
+
+function ataquePatada() {
+    ataqueJugador = "Patada"
+    alert(ataqueJugador)
+}
+
+function ataqueCabezazo() {
+    ataqueJugador = "Cabezazo"
+    alert(ataqueJugador)
+}
+
+function AtaqueEnemigo() {
+    let ataqueAleatorio = aleatorio(1,3)
+
+    if (ataqueAleatorio == 1) {
+        
+    }
+}
+
 
 window.addEventListener("load", iniciarJuego) 
